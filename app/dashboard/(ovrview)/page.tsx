3ffd39b8f1847f17f4b94f2d19ab1915e4ce89a6
tianht <2804622087@qq.com>
2024-04-16 16:11:42 +0800

    fix: fix error

--- app/dashboard/(ovrview)/page.tsx
+++ app/dashboard/(ovrview)/page.tsx
@@ -15,19 +15,15 @@ export default async function Page() {
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
         <Suspense fallback={<CardsSkeleton />}>
-          {/* @ts-expect-error Server Component */}
           <CardWrapper />
         </Suspense>
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        {/* <RevenueChart revenue={revenue}  /> */}
         <Suspense fallback={<RevenueChartSkeleton />}>
           
-          {/* @ts-expect-error Server Component */}
         <RevenueChart />
            
         </Suspense>
-        {/* @ts-expect-error Server Component */}
         <LatestInvoices  />
       </div>
     </main>
